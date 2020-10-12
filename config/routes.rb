7c2cdb0c8c3497517bcc3d8d82bd7bac2babd42b
塩見聡太朗 <shiomisatoshitarou@shiomisatoshitarounoMacBook-Pro.local>
2020-10-12 15:16:00 +0900

    cancel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+  resources :users, only: [:index, :show]
+
   resources :posts
 
   root 'home#index'
