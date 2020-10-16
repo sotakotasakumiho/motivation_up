899fef53b4886bc6dd16bc3983236a3e738e6c54
塩見聡太朗 <shiomisatoshitarou@shiomisatoshitarounoMacBook-Pro.local>
2020-10-16 12:26:09 +0900

    user_post_Linking_changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_102046) do
+ActiveRecord::Schema.define(version: 2020_10_16_030903) do
 
   create_table "posts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
