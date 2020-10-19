a3c5052d75a415623c31f2bd8ffb50091e6a91b1
塩見聡太朗 <shiomisatoshitarou@shiomisatoshitarounoMacBook-Pro.local>
2020-10-19 23:41:30 +0900

    finished_setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   def index
     @users = User.all
   end
@@ -14,8 +17,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        -#flash[:notice]=　"ユーザ登録が完了しました"
-        redirect_to("/users/#{@user.id}")
+      session[:user_id] = @user.id
+      redirect_to("/users/#{@user.id}")
     else
       render :new
     end
@@ -42,9 +45,39 @@ class UsersController < ApplicationController
     redirect_to("/users")
   end
 
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(nickname: params[:nickname], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts")
+    else
+      @error_message = "ニックネームまたはパスワードを入力してください"
+
+      @nickname = params[:nickname]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] ="いたずらはダメだよ"
+      redirect_to("/posts")
+    end
+  end
   private
   def user_params
-    params.require(:user).permit(:name, :nickname, :image)
+    params.require(:user).permit(:name, :nickname, :image, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :nickname, {presence: true}
   validates :image, {presence: true}
+  validates :password, {presence: true}
   mount_uploader :image, ImageUploader
 end
